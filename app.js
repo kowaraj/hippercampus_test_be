46b056ee861b7b2da55097f7fd35869c8ea5a7ce
kowaraj <kowaraj4stuff@gmail.com>
2019-08-16 10:31:22 +0200

    Add upload

--- app.js
+++ app.js
@@ -26,6 +26,38 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/test_be", test_be_router);
 
+// UPLOADING //
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
+app.use('/form', express.static(__dirname + '/form_upload.html'))
+app.post('/upload', function(req, res) {
+  let sampleFile;
+  let uploadPath;
+  if (Object.keys(req.files).length == 0) {
+    res.status(400).send('No files were uploaded.');
+    return;
+  }
+
+  console.log('req.files >>>', req.files); // eslint-disable-line
+  sampleFile = req.files.sampleFile;
+  uploadPath = __dirname + '/uploads/' + sampleFile.name;
+  sampleFile.mv(uploadPath, function(err) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.send('File uploaded to ' + uploadPath);
+  });
+});
+
+// FILE ACCESS //
+app.get('/uploads/*', (req,res) =>{
+  console.log(req.path)
+  
+  res.sendFile(path.join(__dirname + req.path)); 
+});
+
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
